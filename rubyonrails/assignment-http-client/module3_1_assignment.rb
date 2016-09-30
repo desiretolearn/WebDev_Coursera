fb89741ec681a49278ff7ee7d40b8a75fde7ddb4
sushma <sushmahai88@gmail.com>
2016-09-30 17:41:23 -0400

    sdjfhas

---
+++ rubyonrails/assignment-http-client/module3_1_assignment.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+class Recipe
+	include HTTParty
+
+	base_uri "http://food2fork.com/api"
+	default_params key: ENV["FOOD2FORK_KEY"]
+	format :json
+
+	def self.for(search_term)
+		get("/search",query: {q: search_term})["recipes"]
+	end
+end
+
+
